d7f4b1167b5ad349d34f683e59027b15655c5b86
maestro089 <egor-karpov08@bk.ru>
2026-01-27 21:35:10 +0300

    init

---
+++ constant.py
@@ -0,0 +1,74 @@
+from typing import Final
+from datetime import datetime
+
+
+epoch = datetime(1960, 1, 1)
+
+
+endianness_offset: Final = 37
+endianness_length: Final = 1
+encoding_offset: Final = 70
+encoding_length: Final = 1
+align_1_value: Final = 4
+date_created_offset: Final = 164
+date_created_length: Final = 8
+date_modified_offset: Final = 172
+date_modified_length: Final = 8
+
+
+encoding_names: Final = {
+    20: "utf-8",
+    29: "latin1",
+    30: "latin2",
+    31: "latin3",
+    32: "latin4",
+    33: "cyrillic",
+    34: "arabic",
+    35: "greek",
+    36: "hebrew",
+    37: "latin5",
+    38: "latin6",
+    39: "cp874",
+    40: "latin9",
+    41: "cp437",
+    42: "cp850",
+    43: "cp852",
+    44: "cp857",
+    45: "cp858",
+    46: "cp862",
+    47: "cp864",
+    48: "cp865",
+    49: "cp866",
+    50: "cp869",
+    51: "cp874",
+    # 52: "",  # not found
+    # 53: "",  # not found
+    # 54: "",  # not found
+    55: "cp720",
+    56: "cp737",
+    57: "cp775",
+    58: "cp860",
+    59: "cp863",
+    60: "cp1250",
+    61: "cp1251",
+    62: "cp1252",
+    63: "cp1253",
+    64: "cp1254",
+    65: "cp1255",
+    66: "cp1256",
+    67: "cp1257",
+    68: "cp1258",
+    118: "cp950",
+    # 119: "",  # not found
+    123: "big5",
+    125: "gb2312",
+    126: "cp936",
+    134: "euc_jp",
+    136: "cp932",
+    138: "shift_jis",
+    140: "euc-kr",
+    141: "cp949",
+    227: "latin8",
+    # 228: "", # not found
+    # 229: ""  # not found
+}
---
+++ main.py
@@ -0,0 +1,76 @@
+import sys
+import struct
+
+from dataclasses import dataclass
+from typing import Optional 
+from constant import *
+from datetime import datetime, timedelta
+
+
+@dataclass
+class HeaderMetadata:
+    encoding: str = 'utf-8'
+    need_byteswap: str | None = None
+    date_created: str | None = None
+    date_modified: str | None = None
+
+    def __repr__(self):
+        return f"""----------------------------------------------
+        encoding: {self.encoding}
+        need_byteswap: {self.need_byteswap}
+        date_created: {self.date_created}
+        date_modified: {self.date_modified}
+        """
+
+
+class SasRead:
+    def __init__(self, path_file: str):
+        self.byte_order = None
+        self.need_byteswap = None
+
+        self.byte_file = self._open_file(path_file=path_file)
+        self.header_metadata = HeaderMetadata()
+        self._read_metadata()
+        
+
+    def _open_file(self, path_file: str) -> bytes:
+        with open(path_file, 'rb') as f:
+            return f.read()
+        
+    def _read_metadata(self):
+        #Определяем кодировку файла
+        encode = ord(self.byte_file[encoding_offset:encoding_offset + encoding_length])
+        if encode in encoding_names:
+            self.header_metadata.encoding = encoding_names[encode]
+        else:
+            raise ValueError('Ошиибка в определении кодировки файла')
+        
+        buf = self.byte_file[endianness_offset: endianness_offset+endianness_length]
+        if buf == b"\x01":
+            fmt = '<%s' % 'd'
+            align1 = align_1_value
+            self.header_metadata.need_byteswap = sys.byteorder
+        else:
+            fmt = '>%s' % 'd'
+            align1 = 0
+            self.header_metadata.need_byteswap = sys.byteorder
+        
+        #Дата создания таблицы
+        val = self.byte_file[date_created_offset+align1: date_created_offset+date_created_length+align1]
+        val = struct.unpack(str(fmt), val)[0]
+        self.header_metadata.date_created = epoch + timedelta(seconds=val)
+
+        #Дата обновления таблицы
+        val = self.byte_file[date_modified_offset+align1: date_modified_offset+date_modified_length+align1]
+        val = struct.unpack(str(fmt), val)[0]
+        self.header_metadata.date_modified = epoch + timedelta(seconds=val)
+
+
+    def header(self):
+        return self.header_metadata
+        
+
+s = SasRead(path_file='test.sas7bdat')
+
+print(s.header())
+    
